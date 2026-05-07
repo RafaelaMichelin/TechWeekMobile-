ca425c6da7d7c61e44cd77d5f76dfd80454feb41
RafaelaMichelin <RafaelaMichelin@gmail.com>
2026-05-07 19:23:59 -0300

    feat:Criação da Dao de Projetos

---
+++ app/src/main/java/com/br/techweekmobile/dao/ProjectDao.java
@@ -0,0 +1,30 @@
+package com.br.techweekmobile.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.br.techweekmobile.model.Project;
+
+import java.util.List;
+
+@Dao
+public interface ProjectDao {
+    @Insert
+    void insert(Project project);
+
+    @Update
+    void update(Project project);
+
+    @Delete
+    void delete(Project project);
+
+    @Query("SELECT * FROM project")
+    LiveData<List<Project>> getAll();
+
+    @Query("SELECT * FROM project WHERE participant_id = :participantId")
+    LiveData<List<Project>> getByParticipant(int participantId);
+}
