ec962c5858ced50f87ed279e41ec4c86cc283d82
Cohen <arielecohendepaula@gmail.com>
2026-05-06 22:06:59 -0300

    feat: preenchendo a classe main e seus requisitos

--- app/src/main/java/com/br/techweekmobile/ui/MainActivity.java
+++ app/src/main/java/com/br/techweekmobile/ui/MainActivity.java
@@ -1,26 +1,40 @@
 package com.br.techweekmobile.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.br.techweekmobile.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        findViewById(R.id.btn_programacao).setOnClickListener(v ->
+                startActivity(new Intent(this, ProgramacaoActivity.class)));
+
+        findViewById(R.id.btn_cadastro).setOnClickListener(v ->
+                startActivity(new Intent(this, CadastroActivity.class)));
+
+        findViewById(R.id.btn_palestrantes).setOnClickListener(v ->
+                startActivity(new Intent(this, PalestrantesActivity.class)));
+
+        findViewById(R.id.btn_checkin).setOnClickListener(v ->
+                startActivity(new Intent(this, CheckInActivity.class)));
+
+        findViewById(R.id.btn_faq).setOnClickListener(v ->
+                startActivity(new Intent(this, FaqActivity.class)));
+
+        findViewById(R.id.btn_localizacao).setOnClickListener(v ->
+                startActivity(new Intent(this, LocalizacaoActivity.class)));
+
+        findViewById(R.id.btn_admin).setOnClickListener(v ->
+                startActivity(new Intent(this, AdminActivity.class)));
     }
 }
