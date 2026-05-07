96b77500554f777abd88b2c3e3687e58e9db636f
RafaelaMichelin <RafaelaMichelin@gmail.com>
2026-05-06 21:51:38 -0300

    feat:Criando pasta DAO e implementando a classe Participante

--- app/src/main/java/com/br/techweekmobile/dao/ParticipantDao.java
+++ app/src/main/java/com/br/techweekmobile/dao/ParticipantDao.java
@@ -1,4 +1,34 @@
 package com.br.techweekmobile.dao;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.br.techweekmobile.model.Participant;
+import com.br.techweekmobile.model.Speaker;
+
+import java.util.List;
+
+@Dao
 public interface ParticipantDao {
+    //Inserir um participante no Banco
+    @Insert
+    void insert(Participant participant);
+
+    @Update
+    void update(Participant participant);
+
+    @Delete
+    void delete(Participant participant);
+
+    //Busca todos os registros da tabela participant
+    @Query("SELECT * FROM participant")
+    LiveData<List<Participant>> getAll();
+
+
+
 }
+
